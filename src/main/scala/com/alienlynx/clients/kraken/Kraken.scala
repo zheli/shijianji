ac3a592a0331eae474aa0f2fbea76c55879f7a36
Zhe Li <linuxcity.jn@gmail.com>
2018-06-30 23:24:12 +0200

    Fix OHLC response unmarshaller.

--- src/main/scala/com/alienlynx/clients/kraken/Kraken.scala
+++ src/main/scala/com/alienlynx/clients/kraken/Kraken.scala
@@ -1,16 +1,35 @@
 package com.alienlynx.clients.kraken
 
+import java.io
 import java.time.{Instant, ZoneId, ZonedDateTime}
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypeRange, HttpRequest, HttpResponse, MediaType}
+import akka.http.scaladsl.model.MediaTypes.`application/json`
+import akka.http.scaladsl.server.{RejectionError, ValidationRejection}
+import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshal, Unmarshaller}
+import akka.http.scaladsl.util.FastFuture
+import akka.stream.Materializer
+import akka.util.ByteString
+import com.alienlynx.clients.kraken.Kraken._
+import com.alienlynx.utils.{RichFutureResponse, RichUri}
+import com.alienlynx.utils.Json.mapFormat
 import play.api.libs.json._
-import tech.minna.playjson.macros.json
+import play.api.libs.functional.syntax._
+import tech.minna.playjson.macros.{json, jsonFlat}
+import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+import scala.util.control.NonFatal
 
 
 object Kraken {
 
-  @json case class Error(value: String) extends AnyVal
-
-  @json case class ResponseId(value: Int) extends AnyVal
+  @jsonFlat case class Error(value: String) extends AnyVal
+  @jsonFlat case class ResponseId(value: Int) extends AnyVal
+  @jsonFlat case class TradePair(name: String) extends AnyVal
 
   case class OHLC(
     time: ZonedDateTime,
@@ -23,7 +42,18 @@ object Kraken {
     count: Int
   )
 
-  implicit val OHLCEntryReads: Reads[OHLC] = Reads[OHLC] { in =>
+  case class OHLCs(
+    tradePair: TradePair,
+    data: Seq[OHLC],
+    id: ResponseId
+  )
+
+  case class OHLCResponse(
+    error: Seq[Error],
+    result: OHLCs,
+  )
+
+  implicit val OHLCReads: Reads[OHLC] = Reads[OHLC] { in =>
     def timeReads(js: JsValue): ZonedDateTime = {
       js.validate[Int].map { epoch =>
         ZonedDateTime.ofInstant(Instant.ofEpochSecond(epoch), ZoneId.of("UTC"))
@@ -64,10 +94,43 @@ object Kraken {
     }
   }
 
-  case class OHLCResponse(
-    error: Seq[Error],
-    result: Seq[OHLC],
-    last: ResponseId
+  implicit val ohlcsReads: Reads[OHLCs] = Reads[OHLCs] {
+    case o: JsObject => {
+      val id = o.values.last.as[ResponseId]
+      for {
+        pair <- o.keys.headOption.map(TradePair.apply)
+        ohlcs <- o.values.headOption.map(_.as[Seq[OHLC]])
+      } yield JsSuccess(OHLCs(pair, ohlcs, id))
+    }.getOrElse(JsError("Cannot parse OHLC results"))
+
+    case _ => JsError("Cannot parse OHLC results")
+  }
+  implicit val ohlcResponseReads: Reads[OHLCResponse] = Json.reads[OHLCResponse]
+
+  @jsonFlat case class AssetName(value: String) extends AnyVal
+
+  @json case class AssetInfo(
+    aclass: String,
+    altname: String,
+    decimals: Int,
+    display_decimals: Int
+  )
+
+  implicit val assetsFormatter: OFormat[Map[AssetName, AssetInfo]] = mapFormat[AssetName, AssetInfo](string => AssetName(string), _.value)
+
+//  implicit val assetInfoResultMapFormatter: Reads[Map[AssetName, AssetInfo]] = Reads[Map[AssetName, AssetInfo]] { in =>
+//    in.validate[Map[AssetName, AssetInfo]].map { mapItem =>
+//      println(mapItem)
+//      mapItem
+//    }
+//  }
+
+//  implicit val krakenToResponseFormatter: Reads[KrakenResponse] = Reads[KrakenResponse] { in =>
+//  }
+
+  @json case class KrakenResponse(
+    error: Seq[String],
+    result: String
   )
 
 }
--- src/test/scala/com/alienlynx/clients/kraken/KrakenModelSpec.scala
+++ src/test/scala/com/alienlynx/clients/kraken/KrakenModelSpec.scala
@@ -2,9 +2,9 @@ package com.alienlynx.clients.kraken
 
 import java.time.{Instant, ZoneId, ZonedDateTime}
 
-import com.alienlynx.clients.kraken.Kraken.OHLC
+import com.alienlynx.clients.kraken.Kraken.{OHLC, OHLCResponse, OHLCs, ResponseId, TradePair}
 import org.scalatest.{FlatSpec, Matchers}
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, JsValue, Json}
 
 class KrakenModelSpec extends FlatSpec with Matchers {
   "clients.kraken" should "parse OHLC entry in json response" in {
@@ -19,8 +19,46 @@ class KrakenModelSpec extends FlatSpec with Matchers {
       volume = 2.75000000,
       count = 5
     )
-
     Json.parse(json).as[OHLC] shouldBe expectedResult
+
+    val jsonMultiple =
+      """
+        |[
+        | [1530344400,"5473.9","5473.9","5465.1","5472.7","5467.7","2.07331647",13],
+        | [1530344460,"5472.7","5472.7","5471.3","5471.3","5471.3","0.16950000",1]
+        |]
+      """.stripMargin
+    Json.parse(jsonMultiple).as[Seq[OHLC]] shouldBe Seq(
+      OHLC(ZonedDateTime.of(2018,6,30,7,40,0,0,ZoneId.of("UTC")), 5473.9,5473.9,5465.1,5472.7,5467.7,2.07331647,13),
+      OHLC(ZonedDateTime.of(2018,6,30,7,41,0,0,ZoneId.of("UTC")), 5472.7,5472.7,5471.3,5471.3,5471.3,0.16950000,1)
+    )
+  }
+
+  it should "parse OHLC response" in {
+    val json =
+      """
+        |{
+        |  "error": [],
+        |  "result": {
+        |    "XXBTZEUR":[
+        |      [1530344400,"5473.9","5473.9","5465.1","5472.7","5467.7","2.07331647",13],
+        |      [1530344460,"5472.7","5472.7","5471.3","5471.3","5471.3","0.16950000",1]
+        |    ],
+        |    "last": 1530387480
+        |  }
+        |}
+      """.stripMargin
+    Json.parse(json).as[OHLCResponse] shouldBe OHLCResponse(
+      Seq.empty[Kraken.Error],
+      OHLCs(
+        TradePair("XXBTZEUR"),
+        Seq(
+          OHLC(ZonedDateTime.of(2018,6,30,7,40,0,0,ZoneId.of("UTC")), 5473.9,5473.9,5465.1,5472.7,5467.7,2.07331647,13),
+          OHLC(ZonedDateTime.of(2018,6,30,7,41,0,0,ZoneId.of("UTC")), 5472.7,5472.7,5471.3,5471.3,5471.3,0.16950000,1)
+        ),
+        ResponseId(1530387480)
+      )
+    )
   }
 
 }
