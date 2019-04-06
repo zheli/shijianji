8c7d06676eb42d8d622243dba78407b2eee8190f
Zhe Li <linuxcity.jn@gmail.com>
2019-04-06 12:16:48 +0200

    Fix CoinbasePro auth headers.

--- src/main/scala/it/softfork/shijianji/clients/coinbasepro/CoinbasePro.scala
+++ src/main/scala/it/softfork/shijianji/clients/coinbasepro/CoinbasePro.scala
@@ -1,11 +1,12 @@
 package it.softfork.shijianji.clients.coinbasepro
 
+import java.nio.charset.StandardCharsets
 import java.util.{Base64, UUID}
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.model.headers.{Accept, RawHeader}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.Materializer
 import it.softfork.shijianji.utils.RichUri
@@ -16,14 +17,18 @@ import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 import play.api.libs.json.JsonNaming.SnakeCase
 import play.api.libs.json._
-import tech.minna.playjson.macros.jsonFlat
+import tech.minna.playjson.macros.{json, jsonFlat}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
 
 @jsonFlat case class TradeId(value: Int) extends AnyVal
 case class Price(value: Double) extends AnyVal
 case class Size(value: Double) extends AnyVal
 //@jsonFlat case class OrderId(value: UUID) extends AnyVal
+@json case class ErrorResponse(
+  message: String
+)
 
 case class Fill(
   tradeId: TradeId,
@@ -72,16 +77,15 @@ class CoinbasePro(
 ) extends PlayJsonSupport
     with StrictLogging {
 
-  import CoinbasePro.fillReads
 
   // Copied some code from
   // https://github.com/jar-o/gdax-scala/blob/master/src/main/scala/GDAX/api.scala#CoinbaseAuth
-  private def headers(url: Uri, method: String, body: String) = {
+  private def authHeaders(uri: Uri, method: String, body: String) = {
     val timestamp: Long = System.currentTimeMillis / 1000
 
     def signature = {
-      val reqpath: String = url.path.toString
-      val message = timestamp + method.toUpperCase + reqpath + body
+      val requestPathWithQuery: String = uri.path.toString + "?" + uri.query()
+      val message = timestamp + method.toUpperCase + requestPathWithQuery + body
       val hmacKey: Array[Byte] = Base64.getDecoder.decode(apiSecret)
       val secret = new SecretKeySpec(hmacKey, "HmacSHA256")
       val hmac = Mac.getInstance("HmacSHA256")
@@ -90,23 +94,31 @@ class CoinbasePro(
     }
 
     Seq(
+      Accept(MediaTypes.`application/json`),
       RawHeader("CB-ACCESS-KEY", apiKey),
       RawHeader("CB-ACCESS-SIGN", signature),
       RawHeader("CB-ACCESS-TIMESTAMP", timestamp.toString),
-      RawHeader("CB-ACCESS-PASSPHRASE", passphrase)
+      RawHeader("CB-ACCESS-PASSPHRASE", passphrase),
     )
   }
 
-  def fills = {
-    val uri: Uri = sandboxBaseUri / "fills"
-    val futureResponse = Http().singleRequest(HttpRequest(uri = uri))
+  def fills: Future[Seq[Fill]] = {
+    import CoinbasePro.fillReads
+
+    val uri: Uri = (sandboxBaseUri / "fills") ? "product_id=BTC-EUR"
+//    val uri: Uri = (sandboxBaseUri / "accounts")
+    logger.debug(s"Uri: $uri")
+    val request = HttpRequest(uri = uri)
+    val futureResponse = Http().singleRequest(request.withHeaders(request.headers ++ authHeaders(uri, "GET", "")))
     futureResponse.flatMap { response: HttpResponse =>
       if (response.status.isSuccess()) {
-        Unmarshal(response.entity).to[String]
+        response.entity.toStrict(300.millis).map(_.data).map(x => println(x.utf8String))
+        Unmarshal(response.entity).to[Seq[Fill]]
+//        Unmarshal(response.entity).to[Seq[String]]
 //        response.entity.dataBytes.toString()
       } else {
-        logger.error(s"$response")
-        Future.failed(new RuntimeException(s"$response"))
+        Unmarshal(response.entity).to[ErrorResponse].map(x=>logger.error(x.message))
+        throw new RuntimeException(s"$response")
       }
     }
   }
