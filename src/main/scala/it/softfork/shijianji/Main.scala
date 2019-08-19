a1586c040351320b47c6b4d119283de41a7f2e91
Zhe Li <linuxcity.jn@gmail.com>
2019-08-19 19:00:33 +0200

    Add command support.

--- src/main/scala/it/softfork/shijianji/Main.scala
+++ src/main/scala/it/softfork/shijianji/Main.scala
@@ -4,59 +4,51 @@ import java.util.UUID
 
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import com.micronautics.web3j.Address
 import com.typesafe.scalalogging.StrictLogging
-import it.softfork.shijianji.clients.coinbasepro
-import it.softfork.shijianji.clients.coinbasepro.{CoinbasePro, Fill, ProductId}
+import it.softfork.debug4s.DebugMacro._
+import it.softfork.shijianji.clients.etherscan._
 
-import scala.concurrent.Future
-import scala.util.control.NonFatal
-import scala.util.{Failure, Success}
+import scala.concurrent._
+import scala.concurrent.duration._
 
-// NOTE re-use 3rd party client library as much as possible
+
+// re-use 3rd party client library as much as possible
 object Main extends App with StrictLogging {
+
   logger.info("App started.")
 
   implicit val system: ActorSystem = ActorSystem("Shijianji")
   implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(system))
   import system.dispatcher
-
-  val coinbaseproSandboxPass = "INSERT PASSPHASE"
-  val coinbaseproSandboxApiKey = "INSERT KEY"
-  val coinbaseproSandboxApiSecret = "INSERT SECRET"
-
+  val config = ShijianjiConfig.load()
+  // Temporary user until we have a working user module
   val user = User(id = UserId(UUID.randomUUID()), email = "test@ha.com")
 
-  val coinbaseSandbox = CoinbasePro(coinbaseproSandboxApiKey, coinbaseproSandboxApiSecret, coinbaseproSandboxPass, coinbasepro.sandboxBaseUri)
+  try {
+    args.toList match {
+      case Nil =>
+        logger.info("Start API server")
+        // TODO add api server start here
+        sys.exit()
 
-  val resultFuture = for {
-    time <- coinbaseSandbox.time
-    accounts <- coinbaseSandbox.accounts
-    fills <- coinbaseSandbox.allFills
-  } yield {
-    logger.debug(s"Server time: $time")
+      case List("test-run-etherscan-client") =>
+        val etherscan = new Etherscan(config.etherscan)
+        val resultFuture = {
+          etherscan.normalTransactions(Address("0x6BeF00Ee10775d4DD51F1ee2443f17B6f298FC9D")).map(debug)
+        }
+        Await.ready(resultFuture, 1.hours)
+        sys.exit()
 
-    logger.debug(s"Got accounts: $accounts")
-    accounts.foreach(println(_))
-
-    logger.debug(s"Found ${fills.length} filled orders")
-    fills.foreach { fill =>
-      val transaction: Trade = Fill.toTrade(user, fill)
-      println(fill)
-      println(transaction)
-      transaction.fee.foreach(f => logger.debug(s"Fee: $f"))
-      logger.debug(s"Buying price: ${transaction.buyingPrice} ${transaction.soldAmount.currency}")
+      case args =>
+        logger.info(s"Unrecognized command: $args")
+        sys.exit()
     }
+  } catch {
+    case ex: Throwable =>
+      logger.error("Error on application start", ex)
+      // Wait a bit so that the logger have a chance to write to stdout
+      Thread.sleep(100)
+      throw ex
   }
-
-  resultFuture
-    .map { _ =>
-      logger.info("App finished.")
-      system.terminate()
-    }
-    .recover{
-      case NonFatal(ex) =>
-        logger.error(s"wrong!", ex)
-        println("App failed.")
-        system.terminate()
-    }
 }
