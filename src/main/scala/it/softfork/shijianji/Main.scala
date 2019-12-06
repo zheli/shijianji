5d6764b0e440b44f0d4cf6f82dbae5698673e0da
Zhe Li <linuxcity.jn@gmail.com>
2019-12-06 23:17:13 +0100

    Add command to test database.

--- src/main/scala/it/softfork/shijianji/Main.scala
+++ src/main/scala/it/softfork/shijianji/Main.scala
@@ -3,15 +3,30 @@ package it.softfork.shijianji
 import java.util.UUID
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 import com.micronautics.web3j.Address
 import com.typesafe.scalalogging.StrictLogging
 import it.softfork.debug4s.DebugMacro._
+import it.softfork.shijianji.clients.coinbasepro
+import it.softfork.shijianji.clients.coinbasepro._
 import it.softfork.shijianji.clients.etherscan._
+import it.softfork.shijianji.clients.swedbank.Swedbank
+import it.softfork.shijianji.models._
+import it.softfork.shijianji.users.UserPostgresStorage
+import it.softfork.shijianji.utils.Csv
+import slick.basic.DatabaseConfig
+import slick.jdbc
+//import slick.jdbc.H2Profile.api._
+import slick.jdbc.PostgresProfile.api._
+import slick.jdbc.{JdbcProfile, PostgresProfile}
+import slick.lifted.TableQuery
 
 import scala.concurrent._
 import scala.concurrent.duration._
-
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success}
 
 // re-use 3rd party client library as much as possible
 object Main extends App with StrictLogging {
@@ -21,9 +36,11 @@ object Main extends App with StrictLogging {
   implicit val system: ActorSystem = ActorSystem("Shijianji")
   implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(system))
   import system.dispatcher
+
   val config = ShijianjiConfig.load()
+
   // Temporary user until we have a working user module
-  val user = User(id = UserId(UUID.randomUUID()), email = "test@ha.com")
+//  val user = User(id = UUID.randomUUID(), email = "test@ha.com")
 
   try {
     args.toList match {
@@ -32,12 +49,42 @@ object Main extends App with StrictLogging {
         // TODO add api server start here
         sys.exit()
 
+      case List("setup-db") =>
+        val db = Database.forConfig("shijianji.database.postgres2")
+        val userStorage = new UserPostgresStorage("shijianji.database.postgres2")
+//        val users = TableQuery[Users]
+//        val db = Database.forConfig("shijianji.database.h2mem1")
+//        val db2: jdbc.PostgresProfile.backend.Database = Database.forConfig("shijianji.database.postgres2")
+        try {
+          Await.result(userStorage.setup, Duration.Inf)
+          Await.result(userStorage.db.run(
+            DBIO.seq(
+              userStorage.users += User("John Doe", uuid = UUID.randomUUID()),
+              userStorage.users += User("Fred Smith", uuid = UUID.randomUUID()),
+              // print the users (select * from USERS)
+              userStorage.users.result.map(println)
+            )
+          ), Duration.Inf)
+        } finally userStorage.db.close()
+        //        val storage = new Storage(databaseConfig)
+//        val databaseConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig[JdbcProfile]("shijianji.database")
+//        val userRepo = new UserRepository(databaseConfig)
+//        val resultFuture= userRepo.list()
+//          .map(debug(_))
+//          .recover {
+//            case NonFatal(ex) =>
+//              logger.error("Failed", ex)
+//              system.terminate()
+//          }
+//        Await.ready(resultFuture, 1.hour)
+        sys.exit()
+
       case List("test-run-etherscan-client") =>
         val etherscan = new Etherscan(config.etherscan)
         val resultFuture = {
           etherscan.normalTransactions(Address("0x6BeF00Ee10775d4DD51F1ee2443f17B6f298FC9D")).map(debug(_))
         }
-        Await.ready(resultFuture, 1.hours)
+        Await.ready(resultFuture, 1.hour)
         sys.exit()
 
       case args =>
